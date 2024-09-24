2a54e597bb57a4d35bf7d95b9301e7f925176183
divrawat <divrawat2001@gmail>
2024-09-24 18:49:21 +0530

    tf0ht5dyj

--- pages/[chapter].jsx
+++ pages/[chapter].jsx
@@ -218,6 +218,12 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
     const redditUrl = `https://www.reddit.com/submit?url=${encodedUrl}&title=${encodedTitle}`;
 
 
+
+    const handleRedirect = () => {
+        window.open('https://thampolsi.com/4/7457654', '_blank');
+    };
+
+
     const head = () => (
         <Head>
             <title>{`${MANGA_NAME} Chapter ${chapterNumber}`}</title>
@@ -294,7 +300,9 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
                 <div className='mx-3 my-7'>
                     <div className="flex justify-between max-w-[800px] mx-auto md:mb-[50px] mt-5">
                         {previousChapter !== null ? (
-                            <Link href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${previousChapter}`}>
+                            <Link
+                                onClick={handleRedirect}
+                                href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${previousChapter}`}>
                                 <button className="text-[black] text-[13px] hover:scale-105 active:scale-95 transition-transform rounded bg-[white] px-2 py-2 font-semibold">Previous</button>
                             </Link>
                         ) : (
@@ -302,7 +310,9 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
                         )}
 
                         {nextChapter !== null ? (
-                            <Link href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${nextChapter}`}>
+                            <Link
+                                onClick={handleRedirect}
+                                href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${nextChapter}`}>
                                 <button className="text-[black] text-[13px] hover:scale-105 active:scale-95 transition-transform rounded bg-[white] px-2 py-2 font-semibold">Next</button>
                             </Link>
                         ) : (
--- pages/index.jsx
+++ pages/index.jsx
@@ -152,7 +152,9 @@ export default function Home() {
 
 
 
-
+  const handleRedirect = () => {
+    window.open('https://thampolsi.com/4/7457654', '_blank');
+  };
 
 
   return (
@@ -218,7 +220,9 @@ export default function Home() {
 
             {chapters?.map((chapter, index) => (
               <div className="flex hover:scale-105 active:scale-95 transition-transform my-1" key={index}>
-                <a href={chapter.url} className="sm:p-5 p-1.5 text-center  hover:underline text-white bg-black hover:bg-[#2c2b2b] border border-[#292828] rounded sm:w-[160px] w-[110px]">
+                <a
+                  onClick={handleRedirect}
+                  href={chapter.url} className="sm:p-5 p-1.5 text-center  hover:underline text-white bg-black hover:bg-[#2c2b2b] border border-[#292828] rounded sm:w-[160px] w-[110px]">
                   <p className="sm:text-[14px] text-[12px] tracking-wider font-semibold">{`Chapter  ${chapter?.number}`}</p>
                 </a>
               </div>
