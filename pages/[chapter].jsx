6120f70696f2584065ea9589343dfda93e0a5c79
divrawat <divrawat2001@gmail>
2024-09-24 23:14:49 +0530

    sg0er4g

--- pages/[chapter].jsx
+++ pages/[chapter].jsx
@@ -301,7 +301,7 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
                     <div className="flex justify-between max-w-[800px] mx-auto md:mb-[50px] mt-5">
                         {previousChapter !== null ? (
                             <Link
-                                onClick={handleRedirect}
+                                // onClick={handleRedirect}
                                 href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${previousChapter}`}>
                                 <button className="text-[black] text-[13px] hover:scale-105 active:scale-95 transition-transform rounded bg-[white] px-2 py-2 font-semibold">Previous</button>
                             </Link>
@@ -311,7 +311,7 @@ export default function Chapter({ chapterNumber, imageUrls, totalChapters, param
 
                         {nextChapter !== null ? (
                             <Link
-                                onClick={handleRedirect}
+                                // onClick={handleRedirect}
                                 href={`${DOMAIN}/${NEXT_PREVIOUS_PREFIX}-${nextChapter}`}>
                                 <button className="text-[black] text-[13px] hover:scale-105 active:scale-95 transition-transform rounded bg-[white] px-2 py-2 font-semibold">Next</button>
                             </Link>
